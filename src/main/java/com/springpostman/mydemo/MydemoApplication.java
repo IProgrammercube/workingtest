dac721a9468244502aadfb791619add343ce9afc
iprogrammercube <roshan@programmercube.com>
2025-04-06 20:58:29 +0530

    first commit

---
+++ src/main/java/com/springpostman/mydemo/MydemoApplication.java
@@ -0,0 +1,14 @@
+package com.springpostman.mydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydemoApplication.class, args);
+        System.out.print("Roshan Sinha");
+    }
+
+}
